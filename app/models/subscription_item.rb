7c7308f3fd9e9b257ad43d450eb87e5132b2ed3f
k-nasa <htilcs1115@gmail.com>
2018-05-22 22:17:32 +0900

    add validation to SubItemModel

--- app/models/subscription_item.rb
+++ app/models/subscription_item.rb
@@ -1,2 +1,6 @@
 class SubscriptionItem < ApplicationRecord
+  belongs_to :user
+  validates :type, presence: true
+  validates :context, presence: true
+  validates :context, length: { maximum: 50 }
 end
