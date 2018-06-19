b8c0e7919d08d58ecf8309bd6691200facf71080
k-nasa <htilcs1115@gmail.com>
2018-06-19 17:39:57 +0900

    fix/ BookのカラムにNOT NULLを追加

--- db/migrate/20180617152437_create_books.rb
+++ db/migrate/20180617152437_create_books.rb
@@ -1,10 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :author
       t.string :image_url
-      t.date :publish_date
+      t.date :publish_date, null: false
 
       t.timestamps
     end
