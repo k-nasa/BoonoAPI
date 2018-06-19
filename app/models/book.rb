3e6a32aefd6cebf73c9799aaeec6df460c704eb7
k-nasa <htilcs1115@gmail.com>
2018-06-19 18:01:47 +0900

    add validates to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :publish_date, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :publish_date }
+  end
 end
