49d9bdb16cf89da87848da854d291e85e29639be
k-nasa <htilcs1115@gmail.com>
2018-06-19 18:05:30 +0900

    add book factory

---
+++ spec/factories/books.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :book do
+    title 'りゅうおうのお仕事'
+    author '白鳥士郎'
+    image_url 'https://cdn-ak.f.st-hatena.com/images/fotolife/o/ornith/20180209/20180209165259.jpg'
+    publish_date '2018-1-18'
+  end
+end
