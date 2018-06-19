0d77efc15967fbd6451df80575f1a1db93b341f4
k-nasa <htilcs1115@gmail.com>
2018-06-19 18:05:49 +0900

    add valid test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  let(:book) { create(:book) }
+
+  it { expect(book).to be_valid }
+
   describe 'validates' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :publish_date }
