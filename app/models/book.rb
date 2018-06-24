3cba6c606e42c8cffde89166afa32733d75634ab
k-nasa <htilcs1115@gmail.com>
2018-06-24 14:10:55 +0900

    add association to user and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_many :notify_books
   validates :title, presence: true
   validates :publish_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_token
+  has_many :notify_books
   has_many :subscription_items
 end
