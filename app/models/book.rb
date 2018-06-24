459e392efeef4f180bbe29fc9612105cf0c3735d
k-nasa <htilcs1115@gmail.com>
2018-06-24 14:13:43 +0900

    add after_create to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,16 @@ class Book < ApplicationRecord
   has_many :notify_books
   validates :title, presence: true
   validates :publish_date, presence: true
+
+  after_create :create_notify_book
+
+  def create_notify_book
+    TitleItem.all.each do |sub_title|
+      sub_title.user.notify_books.create(book: self) if title.include?(sub_title.content)
+    end
+
+    AuthorItem.all.each do |sub_author|
+      sub_author.user.notify_books.create(book: self) if author.include?(sub_author.content)
+    end
+  end
 end
