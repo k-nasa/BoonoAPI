069f7c47b15e4f2627e074eba4226793348fdc92
k-nasa <htilcs1115@gmail.com>
2018-06-24 15:24:38 +0900

    SubscriptionItem追加時に条件にマッチするBookがあればNotifyBookに追加する

--- app/models/author_item.rb
+++ app/models/author_item.rb
@@ -1,2 +1,9 @@
 class AuthorItem < SubscriptionItem
+  after_create :create_notify_book
+
+  def create_notify_book
+    Book.all.each do |book|
+      user.notify_books.create(book: book) if book.author.include?(content)
+    end
+  end
 end
--- app/models/title_item.rb
+++ app/models/title_item.rb
@@ -1,2 +1,9 @@
 class TitleItem < SubscriptionItem
+  after_create :create_notify_book
+
+  def create_notify_book
+    Book.all.each do |book|
+      user.notify_books.create(book: book) if book.title.include?(content)
+    end
+  end
 end
