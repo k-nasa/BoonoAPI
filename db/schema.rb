5aadbbc8d07d317831cb8542a101ecda4108b3bb
k-nasa <htilcs1115@gmail.com>
2018-06-24 12:43:17 +0900

    $rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623044344) do
+ActiveRecord::Schema.define(version: 20180624033610) do
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180623044344) do
     t.date "publish_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notify_books", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_notify_books_on_book_id"
+    t.index ["user_id"], name: "index_notify_books_on_user_id"
   end
 
   create_table "subscription_items", force: :cascade do |t|
@@ -34,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180623044344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "device_token"
+    t.integer "book_id"
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
