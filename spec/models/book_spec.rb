3aab7ecb6e69f8bf05ea066f1f2199ad4d2bfd7c
k-nasa <htilcs1115@gmail.com>
2018-07-02 13:41:22 +0900

    add book spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  let(:book) { create(:book) }
+  let(:book) { build(:book) }
 
   it { expect(book).to be_valid }
 
   describe 'validates' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :publish_date }
+    it { is_expected.to validate_uniqueness_of(:title).scoped_to(:publish_date) }
   end
 end
