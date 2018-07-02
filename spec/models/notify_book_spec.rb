4ca2001a7fd5429295c9a06b8be97db82a1bed48
k-nasa <htilcs1115@gmail.com>
2018-07-02 16:33:22 +0900

    add notify_book spec

--- spec/models/notify_book_spec.rb
+++ spec/models/notify_book_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NotifyBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it { is_expected.to validate_presence_of :book }
+    it { is_expected.to validate_presence_of :user }
+  end
 end
