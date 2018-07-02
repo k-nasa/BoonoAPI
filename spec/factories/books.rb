361d462eefeecece1e0e7e63d3c7cfeeccdf0925
k-nasa <htilcs1115@gmail.com>
2018-07-02 13:41:02 +0900

    fix/ factories book title

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :book do
-    title 'りゅうおうのお仕事'
+    sequence :title do |n|
+      "りゅうおうのお仕事#{n}"
+    end
+
     author '白鳥士郎'
     image_url 'https://cdn-ak.f.st-hatena.com/images/fotolife/o/ornith/20180209/20180209165259.jpg'
     publish_date '2018-1-18'
