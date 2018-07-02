1f74c687c4bb156cfcadccb59c2ebec78734b016
k-nasa <htilcs1115@gmail.com>
2018-07-02 23:04:27 +0900

    add scope to notify_book model

--- app/models/notify_book.rb
+++ app/models/notify_book.rb
@@ -1,4 +1,10 @@
 class NotifyBook < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :book, presence: true
+  validates :user, presence: true
+
+  scope :today_publish, -> { eager_load(%i(user book)).where('publish_date = ?', Time.zone.today) }
+  scope :today_notify_users, -> { today_publish.map(&:user).uniq }
 end
