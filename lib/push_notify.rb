4655fb20151b65ed1094e278ccfbc1694361aef8
k-nasa <htilcs1115@gmail.com>
2018-07-03 14:40:40 +0900

    push通知用のクラス追加

---
+++ lib/push_notify.rb
@@ -0,0 +1,19 @@
+# push通知用のメソッド郡
+class PushNotify
+  # 発売日に通知
+  def self.publish_date_notify
+    client = Andpush.new(ENV['FIREBASE_SERVER_KEY'])
+
+    users = NotifyBook.today_notify_users
+    users.each do |user|
+      payload = {
+        to: user.device_token,
+        notification: {
+          title: "Boono",
+          body: "本日発売の本があります"
+        }
+      }
+      client.push(payload) if !!user.device_token
+    end
+  end
+end
