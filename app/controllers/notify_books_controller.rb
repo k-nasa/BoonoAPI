169ef115948f3eb860db67f5481237d74f25e843
k-nasa <htilcs1115@gmail.com>
2018-07-04 10:51:20 +0900

    publish_dateでsortする

--- app/controllers/notify_books_controller.rb
+++ app/controllers/notify_books_controller.rb
@@ -1,7 +1,8 @@
 class NotifyBooksController < ApplicationController
   def index
     user = User.find_by(token: params[:token])
-    notify_books = user.notify_books.includes(:book)
+    notify_books = user.notify_books.eager_load(:book)
+    notify_books = notify_books.order('publish_date')
     books = []
     notify_books.each { |notify_book| books << notify_book.book }
 
