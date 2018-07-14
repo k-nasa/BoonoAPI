945a40d194e99c323e346a16e3cc3875ceaddeac
k-nasa <htilcs1115@gmail.com>
2018-07-14 18:10:23 +0900

    add config amazon api

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative 'application'
 
+Amazon::Ecs.options = {
+      AWS_access_key_id: ENV['AWA_ACCESS_KEY_ID'],
+      AWS_secret_key:    ENV['AWA_SECRET_KEY'],
+      associate_tag:     ENV['ACCOSIATE_TAG'],
+      country: :jp
+}
 # Initialize the Rails application.
 Rails.application.initialize!
