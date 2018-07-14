facedf4244b4f6cf13f7d911f0efe44fec9bc813
k-nasa <htilcs1115@gmail.com>
2018-07-14 18:14:18 +0900

    本の詳細データを保存前に取得

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Book < ApplicationRecord
   has_many :notify_books, dependent: :destroy
 
@@ -8,6 +10,21 @@ class Book < ApplicationRecord
 
   before_save :set_book_details
   after_create :create_notify_book
+
+  def set_book_details
+    # cssセレクタで無理やり取ってきているだけなので安定していない
+    opt = {}
+    opt['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+    doc = Nokogiri::HTML.parse(open(detail_url, opt))
+
+    self.big_image_url = image_url.gsub(/._SL160_/, '')
+    self.publisher = doc.css('#dp-container > div:nth-child(27)> table > tr > td > div > ul > li:nth-child(2)').inner_text
+    self.amount = doc.css('#buyNewSection > div > div > span > span').inner_text.slice(/\d+/).to_i
+    self.synopsis = doc.css('#productDescription > p').inner_text
+  rescue OpenURI::HTTPError
+    sleep(1)
+    set_book_details
+  end
 
   def create_notify_book
     TitleItem.all.each do |sub_title|
