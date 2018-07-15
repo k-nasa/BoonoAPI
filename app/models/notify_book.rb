15cf36340a6dff372ee82418cff0d6fb9af5eee4
k-nasa <htilcs1115@gmail.com>
2018-07-15 11:35:36 +0900

    notify_book作成時にuser_infoを更新

--- app/models/notify_book.rb
+++ app/models/notify_book.rb
@@ -7,6 +7,13 @@ class NotifyBook < ApplicationRecord
   validates :user, presence: true
   validates :subscription_item, presence: true
 
+  after_create :user_info_update
+
   scope :today_publish, -> { eager_load(%i(user book)).where('publish_date = ?', Time.zone.today) }
   scope :today_notify_users, -> { today_publish.map(&:user).uniq }
+
+  def user_info_update
+    return if user.new_info
+    user.update!(new_info: true)
+  end
 end
