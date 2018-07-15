544ddc98bdd74637b9c7294d502661f2c2bbc230
k-nasa <htilcs1115@gmail.com>
2018-07-15 12:47:47 +0900

    delete validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
   has_secure_token
   has_many :notify_books, dependent: :destroy
   has_many :subscription_items, dependent: :destroy
-
-  validates :new_info, presence: true
 end
