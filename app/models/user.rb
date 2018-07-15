a5cab5f1748b53ccabad4da28f096061bf36c5de
k-nasa <htilcs1115@gmail.com>
2018-07-15 13:29:19 +0900

    delete set new_info

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,4 @@ class User < ApplicationRecord
   has_secure_token
   has_many :notify_books, dependent: :destroy
   has_many :subscription_items, dependent: :destroy
-
-  before_validation :set_new_info
-
-  def set_new_info
-    self.new_info = false
-  end
 end
