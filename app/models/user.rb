06d345fabd50a71fd18e8f4e370a0b5e9e6da8ce
k-nasa <htilcs1115@gmail.com>
2019-01-05 18:58:30 +0900

    Add validate to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_token
   has_many :notify_books, dependent: :destroy
   has_many :subscription_items, dependent: :destroy
+
+  # TODO iosに対応できたらonにする
+  # validates :device_token, presence: true
 end
