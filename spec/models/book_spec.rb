987fda10aec118559a33de14b1533fa50622aa20
k-nasa <htilcs1115@gmail.com>
2019-01-05 18:58:38 +0900

    Add validation spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Book, type: :model do
 
   describe 'validates' do
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:publish_date) }
+    it { is_expected.to validate_presence_of(:author) }
     it { is_expected.to validate_presence_of(:detail_url) }
-    it { is_expected.to validate_uniqueness_of(:title).scoped_to(:publish_date) }
+    it { is_expected.to validate_presence_of(:publish_date) }
   end
 end
--- spec/models/notify_book_spec.rb
+++ spec/models/notify_book_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe NotifyBook, type: :model do
   describe 'validates' do
     it { is_expected.to validate_presence_of :book }
     it { is_expected.to validate_presence_of :user }
+    it { is_expected.to validate_presence_of :subscription_item }
   end
 end
